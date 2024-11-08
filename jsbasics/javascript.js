6d437274845735b494537a935db0f53f1194b966
Hauken2G <kostasmichopoulos2@gmail.com>
2024-11-08 14:41:05 +0200

    js training

--- jsbasics/javascript.js
+++ jsbasics/javascript.js
@@ -1,7 +1,13 @@
-let text = "Apple, Banana, Kiwi";
-let part = text.slice(-12);
-console.log(part)
 
-
-
-
+let x = "0";
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
+}
+console.log(text)
