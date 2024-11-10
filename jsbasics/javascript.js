dfab69037dc3e9a5553b3cd8aac987a521eabfdc
Hauken2G <kostasmichopoulos2@gmail.com>
2024-11-10 18:48:16 +0200

    js more operator training

--- jsbasics/javascript.js
+++ jsbasics/javascript.js
@@ -5,3 +5,15 @@ for (let x in person) {
   txt += person[x] + " ";
 }
 console.log(txt)
+if (year == 2015) {
+  alert( "That's correct!" );
+  alert( "You're so smart!" );
+}
+
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year == 2015) {
+  alert( 'You guessed it right!' );
+} else {
+  alert( 'How can you be so wrong?' ); // any value except 2015
+}
