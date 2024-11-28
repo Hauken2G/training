1666e1d9a8286d2ff3cc71c11bed01f6954dadc0
Hauken2G <kostasmichopoulos2@gmail.com>
2024-11-28 15:19:39 +0200

    react foundations

---
+++ my-first-react-app/src/components/greeting.jsx
@@ -0,0 +1,9 @@
+const Greetings = () => {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>This is a React component returning JSX.</p>
+      </div>
+    );
+  }
+  export default Greetings
